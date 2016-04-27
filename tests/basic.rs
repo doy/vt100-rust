65b2ce10f83942a9dd2974ac19c694f1c6c9e617
Jesse Luehrs <doy@tozt.net>
2016-04-27 01:50:17 -0400

    split up some tests

--- tests/basic.rs
+++ tests/basic.rs
@@ -6,10 +6,14 @@ mod tests {
 
     #[test]
     fn object_creation() {
-        let mut screen = vt100::Screen::new(24, 80);
+        let screen = vt100::Screen::new(24, 80);
         assert_eq!(screen.rows(), 24);
         assert_eq!(screen.cols(), 80);
+    }
 
+    #[test]
+    fn process_text() {
+        let mut screen = vt100::Screen::new(24, 80);
         let input = b"foo\x1b[31m\x1b[32mb\x1b[3;7;42ma\x1b[23mr";
         screen.process(input);
         assert_eq!(screen.window_contents(0, 0, 0, 50), "foobar\n");
