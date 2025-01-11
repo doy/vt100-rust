ad91587d851336a91f6b34c80af951c96e8148ed
Jesse Luehrs <doy@tozt.net>
2025-01-11 02:15:24 -0500

    fix scroll test

--- tests/scroll.rs
+++ tests/scroll.rs
@@ -117,7 +117,7 @@ fn edge_of_screen() {
 fn scrollback_larger_than_rows() {
     let mut parser = vt100::Parser::new(3, 20, 10);
 
-    parser.process(&gen_nums(1..=10, "\r\n").as_bytes());
+    parser.process(gen_nums(1..=10, "\r\n").as_bytes());
 
     // 1. Extra rows returned
     parser.screen_mut().set_scrollback(4);
@@ -125,12 +125,13 @@ fn scrollback_larger_than_rows() {
 
     // 2. Subtraction overflow
     parser.screen_mut().set_scrollback(10);
-    assert_eq!(parser.screen().contents(), gen_nums(1..=4, "\n"));
+    assert_eq!(parser.screen().contents(), gen_nums(1..=3, "\n"));
 }
 
 #[cfg(test)]
 fn gen_nums(range: RangeInclusive<u8>, join: &str) -> String {
-    range.map(|num| num.to_string())
+    range
+        .map(|num| num.to_string())
         .collect::<Vec<String>>()
         .join(join)
 }
