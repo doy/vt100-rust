93e75c0348fbd7de5617ab8dd9bc64d8584117da
Jesse Luehrs <doy@tozt.net>
2025-01-12 01:00:16 -0500

    optimize cell a bit more

--- src/cell.rs
+++ src/cell.rs
@@ -1,32 +1,31 @@
 use unicode_width::UnicodeWidthChar as _;
 
-const CODEPOINTS_IN_CELL: usize = 6;
-const BYTES_IN_CHAR: usize = 4;
+// chosen to make the size of the cell struct 32 bytes
+const CONTENT_BYTES: usize = 22;
+
+const IS_WIDE: u8 = 0b1000_0000;
+const IS_WIDE_CONTINUATION: u8 = 0b0100_0000;
+const LEN_BITS: u8 = 0b0001_1111;
 
 /// Represents a single terminal cell.
 #[derive(Clone, Debug, Eq)]
 pub struct Cell {
-    contents: [u8; CODEPOINTS_IN_CELL * BYTES_IN_CHAR],
-    // Number of bytes needed to represent all characters
-    num_bytes: u8,
-    // Length of characters
+    contents: [u8; CONTENT_BYTES],
     len: u8,
     attrs: crate::attrs::Attrs,
 }
+const _: () = assert!(std::mem::size_of::<Cell>() == 32);
 
 impl PartialEq<Self> for Cell {
     fn eq(&self, other: &Self) -> bool {
         if self.len != other.len {
             return false;
         }
-        if self.num_bytes != other.num_bytes {
-            return false;
-        }
         if self.attrs != other.attrs {
             return false;
         }
-        let num_bytes = usize::from(self.num_bytes);
-        self.contents[..num_bytes] == other.contents[..num_bytes]
+        let len = self.len();
+        self.contents[..len] == other.contents[..len]
     }
 }
 
@@ -35,18 +34,15 @@ impl Cell {
         Self {
             contents: Default::default(),
             len: 0,
-            num_bytes: 0,
             attrs: crate::attrs::Attrs::default(),
         }
     }
 
-    #[inline]
     fn len(&self) -> usize {
-        usize::from(self.len & 0x0f)
+        usize::from(self.len & LEN_BITS)
     }
 
     pub(crate) fn set(&mut self, c: char, a: crate::attrs::Attrs) {
-        self.num_bytes = 0;
         self.len = 0;
         self.append_char(0, c);
         // strings in this context should always be an arbitrary character
@@ -58,33 +54,27 @@ impl Cell {
 
     pub(crate) fn append(&mut self, c: char) {
         let len = self.len();
-        // This implies that len is at most 5 meaning num_bytes is at most 20
-        // with still enough room for a 4 byte char.
-        if len >= CODEPOINTS_IN_CELL {
+        if len >= CONTENT_BYTES - 4 {
             return;
         }
         if len == 0 {
-            // 0 is always less than 6
             self.contents[0] = b' ';
-            self.num_bytes += 1;
             self.len += 1;
         }
 
-        // we already checked that len < CODEPOINTS_IN_CELL
-        self.append_char(usize::from(self.num_bytes), c);
+        // we already checked that we have space for another codepoint
+        self.append_char(self.len(), c);
     }
 
     // Writes bytes representing c at start
     // Requires caller to verify start <= CODEPOINTS_IN_CELL * 4
     fn append_char(&mut self, start: usize, c: char) {
         c.encode_utf8(&mut self.contents[start..]);
-        self.num_bytes += u8::try_from(c.len_utf8()).unwrap();
-        self.len += 1;
+        self.len += u8::try_from(c.len_utf8()).unwrap();
     }
 
     pub(crate) fn clear(&mut self, attrs: crate::attrs::Attrs) {
         self.len = 0;
-        self.num_bytes = 0;
         self.attrs = attrs;
     }
 
@@ -97,20 +87,19 @@ impl Cell {
     #[allow(clippy::missing_panics_doc)]
     #[must_use]
     pub fn contents(&self) -> &str {
-        std::str::from_utf8(&self.contents[..usize::from(self.num_bytes)])
-            .unwrap()
+        std::str::from_utf8(&self.contents[..self.len()]).unwrap()
     }
 
     /// Returns whether the cell contains any text data.
     #[must_use]
     pub fn has_contents(&self) -> bool {
-        self.len > 0
+        self.len() > 0
     }
 
     /// Returns whether the text data in the cell represents a wide character.
     #[must_use]
     pub fn is_wide(&self) -> bool {
-        self.len & 0x80 == 0x80
+        self.len & IS_WIDE != 0
     }
 
     /// Returns whether the cell contains the second half of a wide character
@@ -118,22 +107,22 @@ impl Cell {
     /// character)
     #[must_use]
     pub fn is_wide_continuation(&self) -> bool {
-        self.len & 0x40 == 0x40
+        self.len & IS_WIDE_CONTINUATION != 0
     }
 
     fn set_wide(&mut self, wide: bool) {
         if wide {
-            self.len |= 0x80;
+            self.len |= IS_WIDE;
         } else {
-            self.len &= 0x7f;
+            self.len &= !IS_WIDE;
         }
     }
 
     pub(crate) fn set_wide_continuation(&mut self, wide: bool) {
         if wide {
-            self.len |= 0x40;
+            self.len |= IS_WIDE_CONTINUATION;
         } else {
-            self.len &= 0xbf;
+            self.len &= !IS_WIDE_CONTINUATION;
         }
     }
 
